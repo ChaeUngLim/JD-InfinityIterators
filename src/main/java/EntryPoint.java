b4a0c3618cfc11ef8afa1adbf3dabe44afed28c6
Awesome <awesome_devnet@outlook.com>
2024-04-27 22:30:38 +0900

    feat(View): combine menu display logic

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,5 +1,10 @@
-public class EntryPoint {
-    public static void main(String[] args) {
+import view.LoginMenu;
 
+public class EntryPoint {
+    private LoginMenu loginMenu;
+    public static void main(String[] args) {
+        EntryPoint entryPoint = new EntryPoint();
+        entryPoint.loginMenu = new LoginMenu();
+        entryPoint.loginMenu.showMenu();
     }
 }

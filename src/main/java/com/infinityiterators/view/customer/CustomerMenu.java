02da8513cf8ed27ccbb3725a21dfd55e80c98a48
kebin0591 <kebin0591@gmail.com>
2024-04-28 23:47:33 +0900

    feat: Implement basic menu for CustomerMenu
    CustomerMenu 클래스 생성
    IMenuStrategyByType 인터페이스 구현
    showMenu() 메서드 구현: 기본 메뉴 표시 및 사용자 입력에 따른 메뉴 이동 처리
    회원 정보 조회, 회원 정보 수정, 주문 내역 조회, 로그아웃 메뉴 항목 표시
    사용자 입력에 따라 해당 메뉴로 이동하거나 로그아웃 처리
    Interaction 클래스 활용하여 사용자 입력 및 메시지 출력 처리
    MenuManager 클래스 활용하여 메뉴 헤더 및 선택 메뉴 표시
    요 커밋은 CustomerMenu의 기본적인 구조를 구현

--- src/main/java/com/infinityiterators/view/customer/CustomerMenu.java
+++ src/main/java/com/infinityiterators/view/customer/CustomerMenu.java
@@ -2,9 +2,46 @@ package com.infinityiterators.view.customer;
 
 import com.infinityiterators.view.*;
 
-public class CustomerMenu implements IMenuStrategyByType {
-    @Override
-    public void showMenu() {
+import com.infinityiterators.view.admin.account.*;
+import com.infinityiterators.view.admin.order.*;
+import com.infinityiterators.view.admin.product.*;
 
+import com.infinityiterators.view.customer.account.CustomerAccountMenu;
+import com.infinityiterators.view.interaction.*;
+
+public class CustomerMenu implements IMenuStrategyByType {
+    public void showMenu() {
+        Interaction.clearScreen();
+
+        MenuManager.displayMenuHeader("고객 메뉴");
+        MenuManager.displaySelectionMenu("상품 조회 및 주문", "주문 내역 조회", "회원 정보 수정(임시)", "로그아웃");
+        while(true) {
+            int selection = Interaction.getInt("메뉴를 선택해주세요");
+
+            switch(selection) {
+                case 1:
+                    // 회원 관리
+                    Interaction.displayMessage("상품 조회 및 주문 메뉴로 이동합니다.", DisplayType.SYSTEM, true);
+                    new CustomerAccountMenu().showEntryPointMenu();
+                    break;
+                case 2:
+                    // 상품 관리
+                    Interaction.displayMessage("주문 내역 조회 메뉴로 이동합니다.", DisplayType.SYSTEM, true);
+                    new AdminProductMenu().showEntryPointMenu();
+                    break;
+                case 3:
+                    // 주문 관리
+                    Interaction.displayMessage("회원 정보 수정 메뉴로 이동합니다.", DisplayType.SYSTEM, true);
+                    new AdminOrderMenu().showEntryPointMenu();
+                    break;
+                case 4:
+                    Interaction.displayMessage("로그아웃합니다.", DisplayType.SYSTEM, true);
+                    // TODO. 로그아웃 처리
+                    return; // 로그아웃 시 메인 메뉴(로그인 화면)로 이동
+                default:
+                    Interaction.displayMessage("잘못된 입력입니다.", DisplayType.ERROR, true);
+                    break;
+            }
+        }
     }
 }

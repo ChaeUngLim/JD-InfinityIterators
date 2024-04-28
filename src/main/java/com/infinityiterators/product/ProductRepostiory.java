a62482119f02ea7dd1b9bb7c48d049db8fcd1046
jisoo78 <ekelldks@naver.com>
2024-04-29 00:52:28 +0900

    #29 feat(Product): create code-frame of product controller

---
+++ src/main/java/com/infinityiterators/product/ProductRepostiory.java
@@ -0,0 +1,18 @@
+package com.infinityiterators.product;
+
+import java.util.ArrayList;
+
+public class ProductRepostiory {
+
+    ArrayList<ProductDto> productDTOArrayList(){return null;}
+
+    public void addProduct(ProductDto dto){}
+
+    public ProductDto findProductByName(String name){
+        return null;
+    }
+
+    public void updateProduct(int index, ProductDto dto){}
+
+    public void removeProduct(){}
+}

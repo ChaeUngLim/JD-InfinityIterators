f3e322edf86d6c1df1e6e50086e4da73abfbad11
kebin0591 <kebin0591@gmail.com>
2024-04-28 23:59:20 +0900

    feat: Introduce CustomerProductMenu for product-related actions
    CustomerProductMenu 클래스를 추가하여 상품 관련 작업을 처리

---
+++ src/main/java/com/infinityiterators/view/customer/product/CustomerProductMenu.java
@@ -0,0 +1,8 @@
+package com.infinityiterators.view.customer.product;
+
+import com.infinityiterators.view.ISubMenuEntryPoint;
+
+public class CustomerProductMenu implements ISubMenuEntryPoint {
+    public void showEntryPointMenu() {
+    }
+}

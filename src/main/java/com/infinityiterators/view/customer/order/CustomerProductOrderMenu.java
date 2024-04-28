2ca8da3759356d6ccee13baf002bc45fb647e96d
kebin0591 <kebin0591@gmail.com>
2024-04-29 00:33:43 +0900

    #20 Refactor: Rename CustomerOrderMenu to: CustomerProductOrderMenu
    보다 구체적으로 표현하기 위해 'Product'를 추가

--- src/main/java/com/infinityiterators/view/customer/order/CustomerProductOrderMenu.java
+++ src/main/java/com/infinityiterators/view/customer/order/CustomerProductOrderMenu.java
@@ -3,7 +3,7 @@ package com.infinityiterators.view.customer.order;
 import com.infinityiterators.view.ISubMenuEntryPoint;
 import com.infinityiterators.view.interaction.Interaction;
 
-public class CustomerOrderMenu implements ISubMenuEntryPoint {
+public class CustomerProductOrderMenu implements ISubMenuEntryPoint {
     public void showEntryPointMenu() {
         Interaction.clearScreen();
     }

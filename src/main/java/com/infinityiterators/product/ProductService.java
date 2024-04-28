d66f6ca89e813f1cb36c1d0b07d5dd7a50745e55
jisoo78 <ekelldks@naver.com>
2024-04-29 00:50:33 +0900

    #36 feat(Product): create code-frame of product controller

---
+++ src/main/java/com/infinityiterators/product/ProductService.java
@@ -0,0 +1,21 @@
+package com.infinityiterators.product;
+
+import java.util.ArrayList;
+
+public class ProductService {
+
+    public boolean checkDuplicateProductName(ProductDto dto){
+        return false;
+    }
+
+
+    public void createProduct(ProductDto dto){}
+
+    public ProductDto searchProductByName(String name){
+        return null;
+    }
+
+    public ArrayList<ProductDto> getAllProduct(){
+        return null;
+    }
+}

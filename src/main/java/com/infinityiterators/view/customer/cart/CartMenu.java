d693494ca2f69cf18906ae649a67cb4bf2376dde
kebin0591 <kebin0591@gmail.com>
2024-04-29 00:43:18 +0900

    #24 Add: Implement CartMenu class for managing customer's cart
        고객의 장바구니를 관리할 수 있는 CartMenu 클래스 구현. 이 클래스를 통해 고객은 자신의 장바구니를 조회, 수정, 삭제하는 기능을 추가 예정.

---
+++ src/main/java/com/infinityiterators/view/customer/cart/CartMenu.java
@@ -0,0 +1,11 @@
+package com.infinityiterators.view.customer.cart;
+
+import com.infinityiterators.view.ISubMenuEntryPoint;
+import com.infinityiterators.view.interaction.Interaction;
+
+public class CartMenu implements ISubMenuEntryPoint {
+    public void showEntryPointMenu() {
+        Interaction.clearScreen();
+    }
+
+}

a4e08b66f0e072dbfcc6bc489afbc151536ca63f
Awesome <awesome_devnet@outlook.com>
2024-04-28 16:37:53 +0900

    refactor(View): refactor implementation classes due to changes in interface names

---
+++ src/main/java/com/infinityiterators/view/admin/product/AdminProductMenu.java
@@ -0,0 +1,10 @@
+package com.infinityiterators.view.admin.product;
+
+import com.infinityiterators.view.*;
+
+public class AdminProductMenu implements ISubMenuEntryPoint {
+    @Override
+    public void showEntryPointMenu() {
+
+    }
+}

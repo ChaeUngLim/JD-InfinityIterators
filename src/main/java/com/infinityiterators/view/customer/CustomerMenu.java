459ab3774164d1007aa16ce0032fc30a04534d8e
kebin0591 <kebin0591@gmail.com>
2024-04-29 12:19:46 +0900

    #23 Refactor: Update CustomerMenu logic and reorder menu options
    로직을 일부 변경, 메뉴 옵션의 순서를 조정을 위한 리팩토링

--- src/main/java/com/infinityiterators/view/customer/CustomerMenu.java
+++ src/main/java/com/infinityiterators/view/customer/CustomerMenu.java
@@ -4,6 +4,7 @@ import com.infinityiterators.view.*;
 
 
 import com.infinityiterators.view.customer.account.*;
+import com.infinityiterators.view.customer.order.CustomerOrderedItemsMenu;
 import com.infinityiterators.view.customer.order.CustomerProductOrderMenu;
 import com.infinityiterators.view.customer.product.CustomerProductMenu;
 import com.infinityiterators.view.interaction.*;
@@ -19,19 +20,19 @@ public class CustomerMenu implements IMenuStrategyByType {
 
             switch(selection) {
                 case 1:
-                    // 회원 관리
+                    // 주문 관리
                     Interaction.displayMessage("상품 조회 및 주문 메뉴로 이동합니다.", DisplayType.SYSTEM, true);
-                    new CustomerAccountMenu().showEntryPointMenu();
+                    new CustomerProductOrderMenu().showEntryPointMenu();
                     break;
                 case 2:
-                    // 상품 관리
+                    // 주문 내역 조회
                     Interaction.displayMessage("주문 내역 조회 메뉴로 이동합니다.", DisplayType.SYSTEM, true);
-                    new CustomerProductMenu().showEntryPointMenu();
+                    new CustomerOrderedItemsMenu().showEntryPointMenu();
                     break;
                 case 3:
-                    // 주문 관리
+                    // 회원 관리
                     Interaction.displayMessage("회원 정보 수정 메뉴로 이동합니다.", DisplayType.SYSTEM, true);
-                    new CustomerProductOrderMenu().showEntryPointMenu();
+                    new CustomerAccountMenu().showEntryPointMenu();
                     break;
                 case 4:
                     Interaction.displayMessage("로그아웃합니다.", DisplayType.SYSTEM, true);

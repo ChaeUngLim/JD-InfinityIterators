ced2f25b5b895ce9933a959772770c1c34521a05
ChaeUngLim <brad213@naver.com>
2024-05-04 10:04:00 +0900

    #60 1.재고수량 체크 2.장바구니 확정 후 비우기 3.구매 확정

--- src/main/java/com/infinityiterators/order/OrderController.java
+++ src/main/java/com/infinityiterators/order/OrderController.java
@@ -5,23 +5,38 @@ import com.infinityiterators.order.OrderDto;
 import com.infinityiterators.order.OrderService;
 import com.infinityiterators.product.ProductDto;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 
 public class OrderController{
 
-//    public void AllSearchAccountDTO() {}
+//    public void AllSearchAccountDTO() {} // 장바구니에서 결제로 이동시 삭제되야해서 담고 있으면 안되서 사용 안함
 
-//    1. 목록정보 모두 출력
+/*주문목록 전체 조회*/
     public ArrayList<OrderDto> displayAllOrderList(){
-        return new OrderService().displayAll();
+        return new OrderService().orderDisplayAll();
     }
 
+/*특정 사용자의 주문목록 조회*/
     public ArrayList<OrderDto> displayUserOrderList(UserDTO user){
-        return new OrderService().
+        return new OrderService().findSomeonesOrderList(user);
     }
 
-    public void addProductToCart(UserDTO user, ProductDto product, int quantity) {
+
+/*장바구니에 있는 항목 구매처리 -> 장바구니 비우기*/
+    // 카트에 있는 상품 하나씩 주문처리
+    public void purchaseProduct() throws Exception {
+        ArrayList<OrderDto> cart = new OrderService().getCart();
+
+        for(OrderDto item : cart) {
+            new OrderService().sellProduct(item);
+        }
+
+        new OrderService().clearCart();
+    }
+
+    public void addProductToCart(UserDTO user, ProductDto product, int quantity) throws Exception {
         OrderDto order = new OrderDto(product, quantity, user);
 
         new OrderService().addProductToCart(order);
@@ -31,9 +46,11 @@ public class OrderController{
         new OrderService().removeOneItemFromCart(dto);
     }
 
-    public void clearCart() {
+    public void clearCart() {   // 일딴 보류
         new OrderService().clearCart();
     }
 
-    
+//    public
+
+
 }
--- src/main/java/com/infinityiterators/order/OrderService.java
+++ src/main/java/com/infinityiterators/order/OrderService.java
@@ -2,21 +2,53 @@ package com.infinityiterators.order;
 
 import com.infinityiterators.Account.UserDTO;
 import com.infinityiterators.OrderRepository;
+import com.infinityiterators.product.ProductDto;
 
 import java.util.ArrayList;
 
 public class OrderService {
 
 
-    public ArrayList<OrderDto> displayAll() {		// 목록정보 모두 출력
+    public ArrayList<OrderDto> orderDisplayAll() {		// 목록정보 모두 출력
         return new OrderRepository().getAllOrders();
     }
 
-    public ArrayList<OrderDto> findSomeonesOrderList(UserDTO dto) {
-        return new OrderRepository().
+    public ArrayList<OrderDto> findSomeonesOrderList(UserDTO user) {
+        return new OrderRepository().getOrdersFromSpecificUser(user);
+    }
+    public boolean isStockInsufficient(ProductDto product, int quantity) {
+        int currentStock = product.getStock(); // 현재 재고수량
+        currentStock -= quantity;
+
+        if(currentStock < 0) { // 음수인 경우 재고부족
+            return true;  // 재고부족
+        } else
+            return false; // 재고 있음
     }
 
-    public void addProductToCart(OrderDto order) {
+    // 장바구니에 담긴 주문서를 하나씩 주문처리하는 로직
+    public void sellProduct(OrderDto order) throws Exception {
+        ProductDto product = order.getProduct(); // 사용자가 구매하려고 하는 제품
+        int quantity = order.getQuantity();     // 사용자가 구매하고 싶은 수량
+
+        boolean isInsufficient = isStockInsufficient(product, quantity);
+        if(isInsufficient) throw new Exception("재고가 부족합니다.");
+
+        // 재고수량 빼기처리
+        product.setStock(product.getStock() - quantity); // 실제 재고 감산처리
+
+        // 주문목록에 옮기기
+        new OrderRepository().addOrder(order);
+    }
+
+    public ArrayList<OrderDto> getCart() {
+        return new OrderRepository().getCart();
+    }
+
+    public void addProductToCart(OrderDto order) throws Exception {
+        boolean isInsufficient = isStockInsufficient(order.getProduct(), order.getQuantity());
+        if(isInsufficient) // 재고부족
+            throw new Exception("재고가 부족합니다.");
         new OrderRepository().addCart(order);
     }
 
@@ -27,6 +59,8 @@ public class OrderService {
     public void clearCart() {
         new OrderRepository().clearCart();
     }
+
+
 
 
 //    public void clearCart() {
